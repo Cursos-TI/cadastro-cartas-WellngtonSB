6e65fa5c34202f00adfd4ead9c3c177da8275efe
WellngtonSB <wellington.souzask8@gmail.com>
2026-03-05 15:46:51 +0000

    add densidade populacional e pib per capita!

--- CartasSuperTrunfo.c
+++ CartasSuperTrunfo.c
@@ -11,7 +11,7 @@ int main() {
   char Estado;
   char NomeCidade[64];
   int PontosTuristicos; 
-  double AreaKM, PIB, Populacao;
+  double DensidadePopulacional, PIBPercapita, AreaKM, PIB, Populacao;
   
   printf("\nEstado: (A a H) : "); // imprime a letra estado A a H 
   scanf("%c", &Estado); // Lê a letra do estado A a H
@@ -23,7 +23,7 @@ int main() {
   scanf(" %[^\n]", NomeCidade); // Lê o nome da cidade, permitindo espaços e limitando a 63 caracteres
 
   printf("\nPopulação: "); // imprime a mensagem para a população
-  scanf("%lF", &Populacao); // Lê a população da cidade, permitindo números decimais
+  scanf("%lf", &Populacao); // Lê a população da cidade, permitindo números decimais
 
   printf("\nArea: "); // imprime a mensagem para a área 
   scanf("%lf", &AreaKM); // Lê a área da cidade em km², permitindo números decimais
@@ -31,19 +31,24 @@ int main() {
   printf("\nPIB: "); // imprime a mensagem para o PIB
   scanf("%lf", &PIB); // Lê o PIB da cidade em bilhões de reais, permitindo números decimais
 
-  printf("\nNumero de pontos turisticos: ");
+  printf("\nNúmero de pontos turisticos: ");
   scanf("%d", &PontosTuristicos);
+  
+  DensidadePopulacional = Populacao / AreaKM;
+  PIBPercapita = PIB / Populacao;
 
     //impressao dos dados cadastrados
-    printf("\n\nCarta cadastrada com sucesso!\n"); 
+    printf("\n\n***** Carta 1 *****\n\n"); 
     printf("Estado: %c\n", Estado);
     printf("Codigo: %s\n", Codigo);
     printf("Nome da Cidade: %s\n", NomeCidade);
     printf("População: %.2lf\n", Populacao); //Imprime a população com 2 casas decimais
     printf("Area: %.2lf km²\n", AreaKM); //Imprime a área com 2 casas decimais e a unidade km²
     printf("PIB: %.2lf bilhões de reais\n", PIB);
-    printf("Número de pontos turisticos: %d\n\n", PontosTuristicos); // Imprime o número de pontos turísticos como um inteiro
+    printf("Número de pontos turisticos: %d\n", PontosTuristicos); // Imprime o número de pontos turísticos como um inteiro
+    printf("Densidade Populacional %.2lf hab/km² \n", DensidadePopulacional);
+    printf("PIB per Capita %.2lf Reais \n\n\n", PIBPercapita);
+
 
 return 0;
 } 
-;
